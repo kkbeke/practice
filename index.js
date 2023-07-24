57e1f9b9e878a61f04deb47c0b10f84d9a4860f3
Carlos Echavez <carlosechavezjr@gmail.com>
2023-07-24 20:01:51 +0800

    click name

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ image2. setAttribute("src", randomImageSource2);
 
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 WINS!";
+    document.querySelector("h1").innerHTML = "Player 1 WINS! CLICK HERE!";
 }
 else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "Player 2 WINS!";
+    document.querySelector("h1").innerHTML = "Player 2 WINS! CLICK HERE!";
 }
 else {
-    document.querySelector("h1").innerHTML = "DRAW!!";
+    document.querySelector("h1").innerHTML = "DRAW!! CLICK HERE!";
 }
 
 function reload() {
